07e32d90b38c05c6323e5477864467fe2408de10
Shawn Toubeau <shawntoubeau@gmail.com>
2021-04-25 14:10:33 -0400

    feat: Adds examples for Go interfaces and go routines

---
+++ channels/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://amazon.com",
+		"http://microsoft.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Printf("%v might be down\n", link)
+		c <- link
+		return
+	}
+
+	fmt.Printf("%v is up\n", link)
+	c <- link
+}
